5be96ed8df1b770a8b8c93d51c86c2fb191a538d
Daerile <palldominiko8@gmail.com>
2024-03-15 02:26:58 +0100

    Fixing filepath error in tests

--- tests/test_graph_system.py
+++ tests/test_graph_system.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import pandas as pd
 import networkx as nx
@@ -6,7 +7,9 @@ from src.backend.graph_system import GraphSystem
 
 class GraphSystemTest(unittest.TestCase):
     def test_create_nodes(self):
-        dataframe = pd.read_csv('files_for_testing/basic_graph.csv', sep=';')
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        filepath = os.path.join(script_dir, 'files_for_testing/basic_graph.csv')
+        dataframe = pd.read_csv(filepath, sep=';')
         gs = GraphSystem(dataframe)
         self.assertEqual(len(gs.digraph.nodes), 5)
         self.assertEqual(len(gs.digraph.edges), 6)
